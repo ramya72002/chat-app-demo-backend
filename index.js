98b87cb8fe547017abe2c62f7c290e1c8842e842
ramya72002 <nsriramya7@gmail.com>
2024-11-08 10:09:37 +0530

    notification sound added

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const connectDB = require('./config/connectDB');
-const router = require('./routes/index'); 
-const cookiesParser = require('cookie-parser');
-const { app, server } = require('./socket/index');
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const connectDB = require('./config/connectDB')
+const router = require('./routes/index')
+const cookiesParser = require('cookie-parser')
+const { app, server } = require('./socket/index')
 
-// const app = express();
-
+// const app = express()
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
-}));
-app.use(express.json());
-app.use(cookiesParser());
+    origin : process.env.FRONTEND_URL,
+    credentials : true
+}))
+app.use(express.json())
+app.use(cookiesParser())
 
-app.get('/', (request, response) => {
+const PORT = process.env.PORT || 8080
+
+app.get('/',(request,response)=>{
     response.json({
-        message: "Server is running"
-    });
-});
-
-// API endpoints
-app.use('/api', router);
-
-// Connect to the database and log success
-connectDB().then(() => {
-    server.listen(()=>{
-        console.log("Database connected successfully");
-
+        message : "Server running at " + PORT
     })
-});
+})
 
-module.exports = app;  // Export the app for Vercel
+//api endpoints
+app.use('/api',router)
+
+connectDB().then(()=>{
+    server.listen(PORT,()=>{
+        console.log("server running at " + PORT)
+    })
+})
