99ab77a4c515d865500fe33c3c95e1b4df459237
ramya72002 <nsriramya7@gmail.com>
2024-11-11 12:12:56 +0530

    next button logics added

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
-const connectDB = require('./config/connectDB')
-const router = require('./routes/index')
-const cookiesParser = require('cookie-parser')
-const { app, server } = require('./socket/index')
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const connectDB = require('./config/connectDB');
+const router = require('./routes/index');
+const cookiesParser = require('cookie-parser');
+const { app, server } = require('./socket/index');
 
-// Middleware
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "https://chat-app-demo-frontend-nine.vercel.app",  // Replace with frontend URL
     credentials: true
-}))
-app.use(express.json())
-app.use(cookiesParser())
+}));
+app.use(express.json());
+app.use(cookiesParser());
 
 // API Endpoints
-app.get('/', (request, response) => {
-    response.json({
-        message: "Server running"
-    })
-})
-app.use('/api', router)
+app.use('/api', router);
 
-// Connect to DB and Start Server
 connectDB().then(() => {
-    server.listen(process.env.PORT || 3000, () => {  // Vercel assigns a port
-        console.log("Server is running")
-    })
-})
+    server.listen(process.env.PORT || 3000, () => {
+        console.log("Server is running");
+    });
+});
